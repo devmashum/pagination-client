1b6ca08bd18a62c73289fb267c924157d7067a30
devmashum <dev.mashum@gmail.com>
2023-11-01 20:32:14 +0100

    new commit

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -10,6 +10,10 @@ const Shop = () => {
     const [cart, setCart] = useState([])
     const { count } = useLoaderData();
     console.log(count);
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+    const numberOfPages = Math.ceil(count / itemsPerPage);
+    const [currentPage, setCurrentPage] = useState(0);
+    const pages = [...Array(numberOfPages).keys()];
 
     useEffect(() => {
         fetch('http://localhost:3000/products')
@@ -63,6 +67,23 @@ const Shop = () => {
         deleteShoppingCart();
     }
 
+    const handleItemsPerPage = (e) => {
+
+        const val = parseInt(e.target.value);
+        console.log(val)
+        setItemsPerPage(val)
+    }
+    const handlePreviousPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1)
+        }
+    }
+
+    const handleNextPage = () => {
+        if (currentPage < pages.length - 1) {
+            setCurrentPage(currentPage + 1)
+        }
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -84,7 +105,23 @@ const Shop = () => {
                     </Link>
                 </Cart>
             </div>
-        </div>
+            <div className='pagination'>
+                <p>Current Page: {currentPage}</p>
+                <button onClick={handlePreviousPage}>Previous</button>
+                {
+                    pages.map(page => <button
+                        className={currentPage === page && 'selected'} onClick={() => setCurrentPage(page)}
+                        key={page}>{page}</button>)
+                }
+                <button onClick={handleNextPage}>Next</button>
+                <select value={itemsPerPage} onChange={handleItemsPerPage} name="" id="">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                </select>
+            </div>
+        </div >
     );
 };
 
